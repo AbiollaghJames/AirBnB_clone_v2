5bca124138560beff50f362a63a8fd69522705dd
AbiollaghJames <abiollajames@protonmail.com>
2023-07-06 06:28:53 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@
 
 
 from fabric.api import run, put, env
-import os
+from os.path import exists
 
 
 env.user = 'ubuntu'
@@ -17,26 +17,22 @@ def do_deploy(archive_path):
     """distribute archive to web servers"""
 
     #if archive doesn't exists return false
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
 
     #file name/path
     f_path = archive_path.split("/")
-    no_extension = f_path[1].split()"."[0]
+    no_extension = f_path[1].split(".")[0]
 
     #Uploading
     put(archive_path, "/tmp")
     run("sudo mkdir -p /data/web_static/releases/" + no_extension + "/")
-    run("sudo tar -xzf /tmp/" no_extension + ".tgz" +
-        " -C /data/web_static/releases/" + no_extension  + "/")
+    run("sudo tar -xzf /tmp/" no_extension + ".tgz" + " -C /data/web_static/releases/" + no_extension  + "/")
     run("sudo rm /tmp/" + no_extension + ".tgz")
-    run("sudo mv /data/web_static/releases/" + no_extension +
-        "/web_static/* /data/web_static/releases/" + no_extension + "/")
-    run("sudo rm -rf /data/web_static/releases/" +
-        no_extension + "/web_static")
+    run("sudo mv /data/web_static/releases/" + no_extension + "/web_static/* /data/web_static/releases/" + no_extension + "/")
+    run("sudo rm -rf /data/web_static/releases/" + no_extension + "/web_static")
     run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s /data/web_static/releases/" + no_extension +
-        "/ /data/web_static/current")
+    run("sudo ln -s /data/web_static/releases/" + no_extension + "/ /data/web_static/current")
     if exists("/data/web_static/releases/" + no_extension):
         return True
     else:
