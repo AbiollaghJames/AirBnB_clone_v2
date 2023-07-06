15b7801e264919358838d8c94504a148ebf13b19
AbiollaghJames <abiollajames@protonmail.com>
2023-07-06 05:22:51 -0700

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+  a Fabric script that distributes
+  an archive to your web servers
+"""
+
+
+from fabric.api import run, put, env
+import os
+
+
+env.user = 'ubuntu'
+env.hosts = ['34.204.61.182','18.207.234.131']
+
+
+def do_deploy(archive_path):
+    """distribute archive to web servers"""
+
+    #if archive doesn't exists return false
+    if not os.path.exists(archive_path):
+        return False
+
+    #file name/path
+    f_path = archive_path.split("/")
+    no_extension = f_path[1].split()"."[0]
+
+    #Uploading
+    put(archive_path, "/tmp")
+    run("sudo mkdir -p /data/web_static/releases/" + no_extension + "/")
+    run("sudo tar -xzf /tmp/" no_extension + ".tgz" +
+        " -C /data/web_static/releases/" + no_extension  + "/")
+    run("sudo rm /tmp/" + no_extension + ".tgz")
+    run("sudo mv /data/web_static/releases/" + no_extension +
+        "/web_static/* /data/web_static/releases/" + no_extension + "/")
+    run("sudo rm -rf /data/web_static/releases/" +
+        no_extension + "/web_static")
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s /data/web_static/releases/" + no_extension +
+        "/ /data/web_static/current")
+    if exists("/data/web_static/releases/" + no_extension):
+        return True
+    else:
+        return False
