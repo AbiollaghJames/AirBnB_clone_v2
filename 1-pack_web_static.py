ab9c6d4787b3a14ae7c2ee923913ef9d90cf5fae
AbiollaghJames <abiollajames@protonmail.com>
2023-07-06 03:43:45 -0700

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+  A Fabric script that generates a .tgz
+  archive from the contents of the web_static folder
+"""
+
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ func to compress file """
+    local("mkdir -p versions")
+    compressed = local("tar -czvf versions/web_static_{}.tgz web_static"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    if compressed.failed:
+        return None
+    return compressed
