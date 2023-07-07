87cc1f5a9530e9cb0ad89d33a5b4c5242dec24db
AbiollaghJames <abiollajames@protonmail.com>
2023-07-07 04:13:28 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,8 +14,8 @@ def do_pack():
     """ func to compress file """
     local("mkdir -p versions")
     date = datetime.now().strftime('%Y%m%d%H%M%S')
-    compressed = local("tar -cvzf versions/web_static_{}.tgz web_static"
-            .format(date))
-    if compressed.failed:
+    comp = local("tar -cvzf versions/web_static_{}.tgz web_static"
+        .format(date))
+    if comp.failed:
         return None
-    return compressed
+    return comp
