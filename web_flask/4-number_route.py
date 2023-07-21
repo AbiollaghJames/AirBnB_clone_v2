6464338018490ee804014d537fb7e85651fac6b8
AbiollaghJames <abiollajames@proonmail.com>
2023-07-21 14:42:24 +0300

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-        """ Displays the text Hello HBNB """
-            return "Hello HBNB!"
+    """ Displays the text Hello HBNB """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
