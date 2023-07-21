518fc9d26df9679ab147f19fac4c78f75e22397a
AbiollaghJames <abiollajames@proonmail.com>
2023-07-21 14:21:50 +0300

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c_text(text):
     return "C {}".format(text)
 
 
-@app.route('/python')
+@app.route('/python/')
 @app.route('/python/<text>')
 def python_text(text='is cool'):
     """ Displays python text """
