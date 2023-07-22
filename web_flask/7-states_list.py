918d8d26637106b5f08f340d887052e9848440f2
AbiollaghJames <abiollajames@proonmail.com>
2023-07-22 09:18:48 +0300

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application """
+
+
+from flask import Flask, render_tamplate
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """ Lists states by ID and name sorted alphabetically """
+    states = sorted(list(storage.all("State").values()), key=lambda y: y.name)
+    return render_tamplate('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def tear_down():
+    """ Removes current SQLAlchemy session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host'0.0.0.0', port=5000)
