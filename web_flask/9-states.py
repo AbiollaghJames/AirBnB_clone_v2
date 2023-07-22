b18a941eb0253733c7da067c6cefb036b4f062cd
AbiollaghJames <abiollajames@protonmail.com>
2023-07-22 01:57:01 -0700

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A Sript that starts a Flask web application """
+""" a script that starts a Flask web application """
 
 
 from flask import Flask, render_template
@@ -10,28 +10,19 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def state_list():
-    """ renders states """
-    states = storage.all('State').values()
-    return render_template('9-states.html', states=states, condition="state_list")
-
-
 @app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
-    """ renders state ID """
-    all_states = storage.all('State')
-    try:
-        state_id = all_states[id]
-        return render_template('9-states.html', state_id=state_id, condition="states_id")
-    except:
-        return render_template('9-states.html', condition="not_found")
+def states(id=None):
+    """ renders states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if id is not None:
+        id = 'State.' + id
+    return render_template('9-states.html', states=states, state_id=id)
 
 
 @app.teardown_appcontext
 def tear_down(self):
     """ Removes current SQLAlchemy session """
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
